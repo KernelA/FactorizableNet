d41c350fa27d7dd44efce7cdbae5976845b7267a
Kang <kangliwei@ufl.edu>
2019-07-23 16:21:58 -0400

    add forward_eval() (identical to forward()) in dataParallel.py

--- models/modules/dataParallel.py
+++ models/modules/dataParallel.py
@@ -35,3 +35,23 @@ class DataParallel(DataParallel_raw):
         replicas = self.replicate(self.module, self.device_ids)
         outputs = self.parallel_apply(replicas, nones, new_inputs)
         return self.gather(outputs, self.output_device)
+    
+    def forward_eval(self, *inputs, **kwargs):
+        assert len(inputs) == 0, "Only support arguments like [variable_name = xxx]"
+        new_inputs = [{} for _ in self.device_ids]
+        for key in kwargs:
+            if key == 'im_data':
+                for i, device in enumerate(self.device_ids):
+                    new_inputs[i][key] = kwargs[key][i].to(device)
+            elif key.startswith("rpn_anchor_targets"):
+                for i, device in enumerate(self.device_ids):
+                    new_inputs[i][key] = [item.to(device) for item in kwargs[key][i]]
+                
+            else:
+                assert isinstance(kwargs[key], list)
+                for i in range(len(self.device_ids)):
+                    new_inputs[i][key] = [kwargs[key][i], ]
+        nones = [[] for _ in self.device_ids]
+        replicas = self.replicate(self.module, self.device_ids)
+        outputs = self.parallel_apply(replicas, nones, new_inputs)
+        return self.gather(outputs, self.output_device)
